0dacae2320b22754b47161089fd7c74234815282
Álvaro Lázaro Gallego <alvaro.lazaro.gallego@stxnext.pl>
2016-02-11 16:15:05 +0100

    Log exceptions

--- lintreview/web.py
+++ lintreview/web.py
@@ -65,7 +65,7 @@ def start_review():
         log.info("Scheduling pull request for %s/%s %s", user, repo, number)
         process_pull_request.delay(user, repo, number, lintrc)
     except:
-        log.error('Could not publish job to celery. Make sure its running.')
+        log.exception('Could not publish job to celery. Make sure its running.')
         return Response(status=500)
     return Response(status=204)
 
